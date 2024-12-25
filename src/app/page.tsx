f6b27bc2387abfa5ec4376ddf62edfdfafa5e0a4
Ariyoly <tianp6211@gmail.com>
2024-12-25 22:47:33 +0700

    update (error-page-upload)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -269,7 +269,7 @@ export default function Home() {
                   size: 'lg',
                   className: 'mx-auto mt-8',
                 })}
-                href='configure/upload'>
+                href='/configure/upload'>
                 Create your case now <ArrowRight className='h-4 w-4 ml-1.5' />
               </Link>
             </div>
